9f7a1a5a0660828b13dfb3aae0604d9a82ddfd92
André Argôlo <argolo@192.168.0.17>
2026-08-17 17:30:31 -0300

    feat(vitest): Configura testes de cobertura para o Vite/React

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/domain/**/*.ts', 'src/infrastructure/**/*.ts'],
+      exclude: ['**/*.test.ts'],
+      thresholds: { statements: 90, branches: 70, functions: 90, lines: 90 },
+    },
+  },
+})
