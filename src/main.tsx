5c1d40dfc87b7073f56a2e4fc1b2e954d63d254c
André Argôlo <argolo@192.168.0.17>
2026-08-17 17:30:04 -0300

    feat: inicializa o aplicativo React com Ionic e renderiza o componente App em src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { setupIonicReact } from '@ionic/react'
+import App from './app/App'
+import '@ionic/react/css/core.css'
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
+import './styles/index.css'
+
+setupIonicReact()
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
