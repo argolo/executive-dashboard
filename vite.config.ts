060b54d8f79cef3800199a64d18a873135c3c4c6
André Argôlo <mail@argolo.dev>
2026-08-17 19:15:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
+import { loadEnv } from 'vite'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      include: ['src/domain/**/*.ts', 'src/infrastructure/**/*.ts'],
-      exclude: ['**/*.test.ts'],
-      thresholds: { statements: 90, branches: 70, functions: 90, lines: 90 },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+  return {
+    base: env.BASE_PATH ?? '/',
+    plugins: [react()],
+    test: {
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        include: ['src/domain/**/*.ts', 'src/infrastructure/**/*.ts'],
+        exclude: ['**/*.test.ts'],
+        thresholds: { statements: 90, branches: 70, functions: 90, lines: 90 },
+      },
     },
-  },
+  }
 })
